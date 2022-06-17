e12b5061252bd50862fc56415c18e1359e6a7ace
kristynerhaugen <kristynerhaugen@gmail.com>
2022-06-16 21:32:09 -0500

    search functionality works for first API fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,10 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-// function to search for new city
-
-// function to persist and click saved cities
-
 //query selector variables
 var searchFormEl = document.querySelector("#search-form");
 var searchButtonEl = document.querySelector("#search-btn");
 var cityInputEl = document.querySelector("#city-name");
 var currentCity = document.querySelector("#current-city");
 var weatherTodayEl = document.querySelector("#weather-today");
+var day = document.querySelector("#current-day");
 var temp = document.querySelector("#temp");
 var wind = document.querySelector("#wind");
 var humidity = document.querySelector("#humidity");
@@ -50,9 +35,6 @@ var formSubmitCity = function (event) {
 
 var getCityInfo = function (cityName) {
     // OpenWeather one call weather API
-    // var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=94.04&exclude=minutely,hourly&appid=a3104bd878f3317330912583ab5d7928";
-    // var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=94.04&appid=a3104bd878f3317330912583ab5d7928";   
-    // var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=boston&APPID=a3104bd878f3317330912583ab5d7928";
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=a3104bd878f3317330912583ab5d7928";
 
     // fetch information from API
@@ -90,22 +72,22 @@ var displayWeather = function(data, cityName) {
 
   var windInfo = data.wind.speed;
   wind.textContent = "Wind Speed: " + windInfo;
+
+  var dayInfo = moment().format("MMMM Do, YYYY");
+  day.textContent = "Today's date: " + dayInfo;
+
+//  started to fetch second API but wasn't successful, this API, once properly fetched, will be used to generate UV index and 5 day forcast 
+//  var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?" +  cityName + "exclude=minutely,hourly&appid=a3104bd878f3317330912583ab5d7928";
+  
+//   fetch(apiUrl2)
+//   .then(function (response) {
+//       // if request is successful
+//       var uvInfo = data.current.uvi;
+//       console.log(data.current.uvi);
+//       //uvIndex.textContent = "UV Index: " + uvInfo;
+//   })
+
 };
 
-
-
 //click event on search function to generate city info when clicked
-
 searchButtonEl.addEventListener('click', formSubmitCity);
-
-
-// click event on search function
-// make sure success capturing CityEl
-// see weather forcast for current city
-// make second API call now that there is access to lat and long
-
-//searchBtn.addEventLIstener('click), function(e) {
-   // var citInEl = docu...
-    //save this in to localStoarge ^^
-   // getCityInfo(cityInputEl.value)
-//}// event listeners so that when saved cities are clicked, information is displayed 
