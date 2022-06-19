f6b0f1378693189d6c03a5a5d8c1f39637e9d324
kristynerhaugen <kristynerhaugen@gmail.com>
2022-06-18 19:20:44 -0500

    successfully have tomorrow's forcast display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-//query selector variables
 var searchFormEl = document.querySelector("#search-form");
 var searchButtonEl = document.querySelector("#search-btn");
 var cityInputEl = document.querySelector("#city-name");
@@ -10,7 +9,12 @@ var wind = document.querySelector("#wind");
 var humidity = document.querySelector("#humidity");
 var uvIndex = document.querySelector("#uv-index");
 var citySearchTerm = document.querySelector('#city-search-term');
-var futureForcast = document.querySelector('#future-forcast');
+//var futureForcast = document.querySelector('#future-forcast');
+var dayOneDate = document.querySelector("#day-one-date");
+var dayOne = document.querySelector("#day-one-temp");
+var dayOneWind = document.querySelector("#day-one-wind");
+var dayOneHumidity = document.querySelector("#day-one-humidity");
+
 
 cityInputEl.value
 
@@ -77,46 +81,59 @@ var displayWeather = function (data, cityName) {
     var dayInfo = moment().format("MMMM Do, YYYY");
     day.textContent = "Today's date: " + dayInfo;
 
-    
 
-var apiKey = "a3104bd878f3317330912583ab5d7928";
-//  fetch second API to generate UV index and 5 day forcast using the latitude and longitude
-var latitude = data.coord.lat;
-console.log(data.coord.lat);
-var longitude = data.coord.lon;
-console.log(data.coord.lon);
-var apiUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
-// this fetch still isn't successful
- fetch(apiUrl2).then(function (response) {
-    console.log(response);
-      // if request is successful
-    if (response.ok) {
-    response.json().then(function (data) {
-        var dailyWeather = data.daily;
-        for (let i = 0; i < dailyWeather.length; i++) {
-            // var day = dailyWeather[i];
-         var uvi = dailyWeather[i].uvi;
-         console.log(`uvi for day ${i}is : ${uvi}`)
+    var apiKey = "a3104bd878f3317330912583ab5d7928";
+    //  fetch second API to generate UV index and 5 day forcast using the latitude and longitude
+    var latitude = data.coord.lat;
+    console.log(data.coord.lat);
+    var longitude = data.coord.lon;
+    console.log(data.coord.lon);
+    var apiUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units={imperial}&appid=${apiKey}`;
 
-        uvIndex.textContent = `UV Index: ${uvi}`;
-        // var forcast = dailyWeather[i].
-        // futureForcast.textContent = `The seven day forcast is ${forcast}`;
+    // this fetch still isn't successful
+    fetch(apiUrl2).then(function (response) {
+        console.log(response);
+        // if request is successful
+        if (response.ok) {
+            response.json().then(function (data) {
+                var dailyWeather = data.daily;
+                for (let i = 0; i < dailyWeather.length; i++) {
+                    // var day = dailyWeather[i];
+                    // fetching and diplaying uv index
+                    var uvi = dailyWeather[0].uvi;
+                    console.log(`uvi for day ${i}is : ${uvi}`);
+                    uvIndex.textContent = `UV Index: ${uvi}`;
+
+                    // fetching and displaying day one temp
+                    var dayOneForcast = dailyWeather[1].temp.day;
+                    console.log(`temperature for day ${1} is: ${dayOneForcast}`);
+                    dayOne.textContent = `Temperature in Kelvin: ${dayOneForcast}`;
+
+                    // fetching and displaying day one wind
+                    var dayOneWindSpeed = dailyWeather[1].wind_speed;
+                    console.log(`wind speed ${1} is: ${dayOneWindSpeed}`);
+                    dayOneWind.textContent = `Wind Speed: ${dayOneWindSpeed}`;
+
+                    // fetching and displaying day one humidity
+                    var dayOneHumid = dailyWeather[1].humidity;
+                    console.log(`Humidity ${1} is: ${dayOneHumid}`);
+                    dayOneHumidity.textContent = `Humidity: ${dayOneHumid}`;
+
+                    // var forcast = dailyWeather[i].
+                    // futureForcast.textContent = `The seven day forcast is ${forcast}`;
+                }
+                console.log("daily weather is ", dailyWeather);
+            });
         }
-        console.log("daily weather is ", dailyWeather);
+        else {
+            alert("Alert--there was an issue with request for weather.");
+        }
 
-        
-
-         });
-    }
-    else {
-        alert("Alert--there was an issue with request for weather.");
-    }
-    
-    //   var uvInfo = data.current.uvi;
-    //   console.log(data.current.uvi);
-      //uvIndex.textContent = "UV Index: " + uvInfo;
- })
+        //   var uvInfo = data.current.uvi;
+        //   console.log(data.current.uvi);
+        //uvIndex.textContent = "UV Index: " + uvInfo;
+    })
 };
 //click event on search function to generate city info when clicked
 searchButtonEl.addEventListener('click', formSubmitCity);
